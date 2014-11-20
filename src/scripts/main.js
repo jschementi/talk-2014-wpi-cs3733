d81742fd6f8f4b58898d3683483a2e014c1b0a8c
Jimmy Schementi <jschementi@gmail.com>
2014-11-19 23:51:35 -0500

    Add Google Analytics

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,7 +8,8 @@ var bespoke = require('bespoke'),
   scale = require('bespoke-scale'),
   hash = require('bespoke-hash'),
   progress = require('bespoke-progress'),
-  forms = require('bespoke-forms');
+  forms = require('bespoke-forms'),
+  ga = require('bespoke-ga');
 
 // Bespoke.js
 bespoke.from('article', [
@@ -20,7 +21,8 @@ bespoke.from('article', [
   scale(),
   hash(),
   progress(),
-  forms()
+  forms(),
+  ga({trackingId: 'UA-45861154-1'})
 ]);
 
 // Prism syntax highlighting
